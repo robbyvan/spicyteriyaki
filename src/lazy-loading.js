e1092116ad2bc6870ced7b02e25275718a7fe845
Robby <fzpinus@gmail.com>
2018-08-07 10:15:23 +0800

    feat: simple lazy-loading

---
+++ src/lazy-loading.js
@@ -0,0 +1,24 @@
+lazyLoad();
+
+window.addEventListener('scroll', e => {
+    lazyLoad();
+})
+
+function lazyLoad() {
+  var lazyImage = document.getElementsByClassName('lazy') ;
+  for (let i = 0; i < lazyImage.length; ++i) {
+    if (elemenInViewport(lazyImage[i])) {
+      lazyImage[i].setAttribute('src', lazyImage[i].getAttribute('data-src'));
+    }
+  }
+}
+
+function elemenInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+  );
+}
